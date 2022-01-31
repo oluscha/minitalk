02dfccdb513823bd0b85a5188cf9faaabbb935c0
oluscha <94226815+oluscha@users.noreply.github.com>
2022-01-31 17:28:11 +0300

    Add files via upload

---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lschwart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 13:52:42 by lschwart          #+#    #+#             */
+/*   Updated: 2021/10/10 13:52:49 by lschwart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include <limits.h>
+
+#endif
